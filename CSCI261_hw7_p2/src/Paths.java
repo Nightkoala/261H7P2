e09979d89c72a9210ac40962204aca4b6df55733
Derek Brown <djb3718@rit.edu>
2014-12-06 21:31:11 -0500

    Added repo to eclipse and created java file

---
+++ CSCI261_hw7_p2/src/Paths.java
@@ -0,0 +1,22 @@
+/**
+ * Paths.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ *
+ * Purpose:	Find the minimum distance from u to v for every pair of vertices.
+ */
+
+import java.util.Scanner;
+
+public class Paths {
+
+	// Attributes
+	
+	// Constructor
+	
+	// Methods
+	
+	public static void main(String[] args) {
+
+	}//end main
+}//end Paths class
